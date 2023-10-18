8e72b6d53ed902617e8cdf3f2ecc2ac47a54c122
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-17 19:23:24 -0600

    Version 16

--- login.php
+++ login.php
@@ -11,9 +11,10 @@
         $resultados2=$dao2->insertarConsulta($consulta2,$parametros);
         echo $resultados2;  
         if($resultados>0){
-            header("Location: http://157.245.253.25/Maestros/materias.php?matricula=$matricula");
+            echo $resultados;
+            //header("Location: http://157.245.253.25/Maestros/materias.php?matricula=$matricula");
         }else if($resultados2>0){
-            header("Location: http://157.245.253.25/Alumnos/dahs.php?matricula=$matricula");
+            header("Location: http://157.245.253.25/Alumnos/dash.php?matricula=$matricula");
         }else{
             header("Location: http://157.245.253.25/login.php");
         } 
