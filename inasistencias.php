47f2734d9a213d56a7268cebaff84d0def103d00
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-19 04:12:39 -0600

    Version 128

--- inasistencias.php
+++ inasistencias.php
@@ -9,7 +9,7 @@
     $grupo = 2;
     $clase = "Matematicas";
     $daoEvento = new DAO();
-    $sentencia1 = "Create EVENT $nombre ON SCHEDULE EVERY $valor MINUTES STARTS '2023-10-19 03:38:00' DO BEGIN INSERT INTO Pase_de_lista (Matricula,Asistio,Dia,Hora,Grupo,Clase)"."VALUES (:matricula,:asistencia,:dia,:hora,:grupo,:clase); END";
+    $sentencia1 = "Create EVENT $nombre ON SCHEDULE EVERY $valor MINUTES STARTS '2023-10-19 03:38:00' DO BEGIN INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES (:matricula,:asistencia,:dia,:hora,:grupo,:clase) END";
     $parametros = array("matricula"=>$matricula,"asistio"=>$asistencia,"dia"=>$dia,"grupo"=>$grupo,"clase"=>$clase);
     $daoEvento->insertarConsulta($sentencia1,$parametros);
 ?>
