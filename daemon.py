458f092542f5a4975d9a0fe6d1258f93f8ff1726
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-25 19:41:47 -0600

    Daemon 9

--- daemon.py
+++ daemon.py
@@ -4,7 +4,7 @@ print('getcwd: ', os.getcwd())
 print('__file__:   ',__file__)
 import subprocess
 
-ruta = "/var/www/html/lista.php"
+ruta = "/var/www/html/lista_daemon.py"
 time.sleep(60)
 """while aux == 0:
 time.sleep(86400)
