593cc4161f6257a72be4c8788e237ea513f26697
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-25 19:18:19 -0600

    Daemon 6

--- daemon.py
+++ daemon.py
@@ -2,7 +2,8 @@ import os
 import time
 print('getcwd: ', os.getcwd())
 print('__file__:   ',__file__)
+import subprocess
 
+ruta = "/var/www/html/lista_daemon.py"
 time.sleep(60)
-
-python3 lista_daemon.py
+subprocess.run(['python3',ruta])
