de6440e5d9beefa3552f9b2c3b2dc3416625931d
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-27 12:31:01 -0600

    version 29

--- daemon.py
+++ daemon.py
@@ -6,9 +6,10 @@ import subprocess
 aux=0
 ruta = "/var/www/html/lista_daemon.py"
 #time.sleep(60)
+
 while aux == 0:
     time.sleep(1)
 
     #time.sleep(86400)
-    
+
     subprocess.run(['python3',ruta])
