38664b2c5085f4b34a70587c66d34eaed2667973
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-27 12:30:00 -0600

    version 29

--- daemon.py
+++ daemon.py
@@ -4,10 +4,10 @@ print('getcwd: ', os.getcwd())
 print('__file__:   ',__file__)
 import subprocess
 aux=0
-ruta = "/var/www/html/lista_daemon.php"
+ruta = "/var/www/html/lista_daemon.py"
 #time.sleep(60)
 while aux == 0:
     time.sleep(1)
-    
+
     #time.sleep(86400)
     subprocess.run(['python3',ruta])
