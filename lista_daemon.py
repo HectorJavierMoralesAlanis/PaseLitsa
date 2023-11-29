acc2d8169017fbdcee6db5fb1a702b51feaae171
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-29 11:50:17 -0600

    version 31

--- lista_daemon.py
+++ lista_daemon.py
@@ -4,24 +4,26 @@ import mysql.connector
 dt = datetime.now()
 ts = datetime.timestamp(dt)
 
-str_date_time = dt.strftime("%d-%m-%Y")
-str_time = dt.strftime("%I%p %M:%S")
+str_date_time = date_time.strftime("%d-%m-%Y")
+str_time = date_time.strftime()
 # Replace these values with your own database connection details
 db_config = {
     # Replace these values with your MySQL server credentials
     'host': 'localhost',
     'user': 'admin',
     'password': '3d54d0c824c2c3f7b4415dff96ed0fbeb752c151b68224dc',
-    }
+    'database': 'lista'
+}
 
 try:
     # Establish a connection to the MySQL server
     connection = mysql.connector.connect(**db_config)
+
     # Create a cursor object to interact with the database
     cursor = connection.cursor()
 
     # Example SELECT query
-    query = "INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES ('2030103','0',str_time,str_time,'1','Matematicas')"
+    query = "INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES ('2030103','0','[value-4]','[value-5]','[value-6]','[value-7]')"
     cursor.execute(query)
     # Fetch all rows from the result set
     result = cursor.fetchall()
