0d2d47785dd5ae7f2d8798e8fb53a95b157285c4
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-29 11:53:25 -0600

    version 32

--- lista_daemon.py
+++ lista_daemon.py
@@ -23,7 +23,7 @@ try:
     cursor = connection.cursor()
 
     # Example SELECT query
-    query = "INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES ('2030103','0','[value-4]','[value-5]','[value-6]','[value-7]')"
+    query = "INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES ('2030103','0',str_time,str_time,'1','Matematicas')"
     cursor.execute(query)
     # Fetch all rows from the result set
     result = cursor.fetchall()
