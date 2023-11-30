ef10f4443e66b15fdc7d2088d8947d9b6e7bd75a
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-30 12:37:53 -0600

    version 41

--- lista_daemon.py
+++ lista_daemon.py
@@ -4,11 +4,9 @@ import mysql.connector
 dt = datetime.now()
 ts = datetime.timestamp(dt)
 
-str_date_time = dt.datetime("%d-%m-%Y")
-print(type(str_date_time))
-fecha = dt.strptime(str_data_time,"%Y-%m-%d")
-print(type(fecha))
-str_time = dt.datetime("%I %M:%S")
+#str_date_time = dt.datetime("%d-%m-%Y")
+#print(str_date_time)
+#str_time = dt.datetime("%I %M:%S")
 #print(str_time)
 # Replace these values with your own database connection details
 db_config = {
@@ -28,7 +26,7 @@ try:
     cursor = connection.cursor()
 
     # Example SELECT query
-    query = "INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES ('2030103','0','{dt.datetime}','{str_time}','1','Matematicas')"
+    query = "INSERT INTO `Pase_de_lista`(`Matricula`, `Asistio`, `Dia`, `Hora`, `Grupo`, `Clase`) VALUES ('2030103','0','{dt.}','{str_time}','1','Matematicas')"
     cursor.execute(query)
     # Fetch all rows from the result set
     result = cursor.fetchall()
