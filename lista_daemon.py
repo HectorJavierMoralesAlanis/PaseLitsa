20416b00c0b246c9a0c62e0fc2a4b0a4259961fe
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-30 12:40:52 -0600

    version 42

--- lista_daemon.py
+++ lista_daemon.py
@@ -4,7 +4,7 @@ import mysql.connector
 dt = datetime.now()
 ts = datetime.timestamp(dt)
 
-str_date_time = dt.datetime("%d-%m-%Y")
+str_date_time = dt.today("%d-%m-%Y")
 print(type(str_date_time))
 #str_time = dt.datetime("%I %M:%S")
 #print(str_time)
