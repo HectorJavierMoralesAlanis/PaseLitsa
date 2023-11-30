0d808eda6c5e791c69674b6328b1ac72f9748de4
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-11-30 12:51:10 -0600

    version 48

--- lista_daemon.py
+++ lista_daemon.py
@@ -6,7 +6,7 @@ import mysql.connector
 
 fecha = date.today()
 print(fecha)
-hora = date.today().time()
+hora = date.datetime.now().time()
 print(hora)
 # Replace these values with your own database connection details
 db_config = {
